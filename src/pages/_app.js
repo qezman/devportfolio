cc80b186ec2d1ad2842df2146bebf5679d214243
Kazeem Jimoh <holaryinka5050@gmail.com>
2025-12-31 11:40:45 +0100

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -23,8 +23,8 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes"
         />
-        <meta property="og:image" content="/1.jpg" />
-        <link rel="icon" href="/5.jpg" />
+        <meta property="og:image" content="/logo.svg" />
+        <link rel="icon" href="/logo.svg" />
         <meta name="theme-color" content="#1f2937" />
         <meta
           name="description"
